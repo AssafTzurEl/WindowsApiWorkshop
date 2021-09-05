b8b746672728e9cd7598445af13a5eeb86c3637e
Assaf Tzur-El <28855903+AssafTzurEl@users.noreply.github.com>
2021-09-05 11:06:13 +0300

    Add handles example

--- Handles/Handles.cpp
+++ Handles/Handles.cpp
@@ -1,19 +1,13 @@
 #include <Windows.h>
 #include <iostream>
-
-void CreateEvent_NoErrors()
-{
-    std::cout << "\nCreating a normal event." << std::endl;
-    HANDLE eventHandle = CreateEvent(nullptr, false, false, nullptr);
-
-    std::cout << "Event created successfully? " << (eventHandle != NULL) << std::endl;
-
-    CloseHandle(eventHandle);
-}
+#include <string>
 
 std::string GetLastWinApiError()
 {
     DWORD err = ::GetLastError();
+
+    //return std::to_string(err);
+
     LPVOID lpMsgBuf;
 
     ::FormatMessageA(
@@ -45,7 +39,27 @@ void CloseEventHandle(HANDLE eventHandle)
     }
     else
     {
-        std::cout << "Event closed with error: " << GetLastWinApiError() << std::endl;
+        std::string error = GetLastWinApiError();
+
+        std::cout << "Event closed with error: " << error << std::endl;
+    }
+}
+
+void CreateEvent_NoErrors()
+{
+    std::cout << "\nCreating a normal event." << std::endl;
+    HANDLE eventHandle = CreateEvent(nullptr, false, false, nullptr);
+
+    if (eventHandle != NULL)
+    {
+        std::cout << "Event created successfully" << std::endl;
+
+        CloseEventHandle(eventHandle);
+    }
+    else
+    {
+        DWORD error = GetLastError();
+        std::cout << "Error #" << error << ": Event not created" << std::endl;
     }
 }
 
@@ -55,27 +69,55 @@ void CreateEvent_AlreadyExists()
     const TCHAR *eventName = TEXT("myevent");
 
     HANDLE event1 = CreateEvent(nullptr, false, false, eventName);
-    std::cout << "Last error: " << GetLastWinApiError() << std::endl;
+    std::string error = GetLastWinApiError();
+
+    std::cout << "Last error: " << error << std::endl;
 
     HANDLE event2 = CreateEvent(nullptr, false, false, eventName);
-    std::cout << "Last error: " << GetLastWinApiError() << std::endl;
+
+    error = GetLastWinApiError();
+    std::cout << "Last error: " << error << std::endl;
 
     CloseEventHandle(event2);
 
     CloseEventHandle(event1);
 }
 
-void CloseEvent_ShouldFail()
+void CloseNullHandle_ShouldFail()
 {
     std::cout << "\nClosing a null event." << std::endl;
     CloseEventHandle(NULL);
 }
 
+void CloseHandleTwice_ShouldFail()
+{
+    std::cout << "\nCreating an event and closing it twice." << std::endl;
+    HANDLE eventHandle = CreateEvent(nullptr, false, false, nullptr);
+
+    if (eventHandle != NULL)
+    {
+        std::cout << "Event created successfully" << std::endl;
+
+        std::cout << "Closing once:" << std::endl;
+        CloseEventHandle(eventHandle);
+        std::cout << "Closing twice:" << std::endl;
+        CloseEventHandle(eventHandle); // Crash!? RTFM!
+    }
+    else
+    {
+        std::string error = GetLastWinApiError();
+
+        std::cout << "Last error: " << error << std::endl;
+    }
+}
+
+
 int main()
 {
     CreateEvent_NoErrors();
     CreateEvent_AlreadyExists();
-    CloseEvent_ShouldFail();
+    CloseNullHandle_ShouldFail();
+    CloseHandleTwice_ShouldFail();
 
     return 0;
 }
